0abb3e231ffe1c5f252e43c1a965fe72152b45b9
Hannah Kwon <hkwon1@g.ucla.edu>
2023-03-15 21:47:43 -0700

    Change the button icons for joke, angry and drunk

--- src/twitter_toolbar_version.ts
+++ src/twitter_toolbar_version.ts
@@ -143,6 +143,14 @@ function getFeaturedTweet() {
   const botSvg =
     '<svg class=\'inline-block ml-2 mb-1\' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="transform: ;msFilter:;"><path d="M21 10.975V8a2 2 0 0 0-2-2h-6V4.688c.305-.274.5-.668.5-1.11a1.5 1.5 0 0 0-3 0c0 .442.195.836.5 1.11V6H5a2 2 0 0 0-2 2v2.998l-.072.005A.999.999 0 0 0 2 12v2a1 1 0 0 0 1 1v5a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-5a1 1 0 0 0 1-1v-1.938a1.004 1.004 0 0 0-.072-.455c-.202-.488-.635-.605-.928-.632zM7 12c0-1.104.672-2 1.5-2s1.5.896 1.5 2-.672 2-1.5 2S7 13.104 7 12zm8.998 6c-1.001-.003-7.997 0-7.998 0v-2s7.001-.002 8.002 0l-.004 2zm-.498-4c-.828 0-1.5-.896-1.5-2s.672-2 1.5-2 1.5.896 1.5 2-.672 2-1.5 2z"></path></svg>';
 
+  const madSvg =
+    '<svg class=\'inline-block ml-2 mb-1\' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill:;transform: ;msFilter:;"><path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm-5 8.5.002-.022-1.373-.549.742-1.857 5 2-.742 1.857-1.031-.413c-.014.014-.023.031-.037.044A1.499 1.499 0 0 1 7 10.5zM8 17s1-3 4-3 4 3 4 3H8zm8.986-6.507c0 .412-.167.785-.438 1.056a1.488 1.488 0 0 1-2.112 0c-.011-.011-.019-.024-.029-.035l-1.037.415-.742-1.857 5-2 .742 1.857-1.386.554a.036.036 0 0 1 .002.01z"></path></svg>';
+
+  const jokeSvg =
+    '<svg class=\'inline-block ml-2 mb-1\' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill:;transform: ;msFilter:;"><path d="M14 19v-4h-4v4c0 1.103.897 2 2 2s2-.897 2-2z"></path><path d="M12 2C6.486 2 2 6.486 2 12c0 4.434 2.903 8.198 6.906 9.505A3.969 3.969 0 0 1 8 19v-2.499C6.412 15.027 6 13 6 13h12s-.411 2.027-2 3.501V19c0 .953-.349 1.816-.906 2.504C19.097 20.197 22 16.434 22 12c0-5.514-4.486-10-10-10zm-3.5 9a1.5 1.5 0 1 1 .001-3.001A1.5 1.5 0 0 1 8.5 11zm4.5 0s.5-2 2.5-2c1.999 0 2.5 2 2.5 2h-5z"></path></svg>';
+
+  const drunkSvg =
+    '<svg class=\'inline-block ml-2 mb-1\' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill:;transform: ;msFilter:;"><path d="M20 6h-2V4a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v15c0 1.654 1.346 3 3 3h10c1.654 0 3-1.346 3-3v-1h2c1.103 0 2-.897 2-2V8c0-1.103-.897-2-2-2zM8 17H6V7h2v10zm6 0h-2V7h2v10zm6-1h-2V8h2v8z"></path></svg>';
   function setupButtons() {
     const tweet = getFeaturedTweet();
     // This stops the write tweet button from having the ai buttons
@@ -158,9 +166,9 @@ function getFeaturedTweet() {
       `<div class='ai_button_wrapper flex justify-start text-gray-500 items-center mt-4 gap-2'>
            <div>AI replies:</div>
            <button type='button' data-tone="default" class='ai_button  text-sm rounded-lg py-1 px-2 border-2 border-gray-400 dark:border-gray-400 disabled:cursor-progress fill-gray-500'>${loaderSvg}<span>Default</span>${botSvg}</button>
-           <button type='button' data-tone="joke" class='ai_button  text-sm rounded-lg py-1 px-2 border-2 border-gray-400 dark:border-gray-400 disabled:cursor-progress fill-gray-500'>${loaderSvg}<span>Joke</span>${botSvg}</button>
-           <button type='button' data-tone="mad" class='ai_button  text-sm rounded-lg py-1 px-2 border-2 border-gray-400 dark:border-gray-400 disabled:cursor-progress fill-gray-500'>${loaderSvg}<span>Angry</span>${botSvg}</button>
-           <button type='button' data-tone="drunk" class='ai_button  text-sm rounded-lg py-1 px-2 border-2 border-gray-400 dark:border-gray-400 disabled:cursor-progress fill-gray-500'>${loaderSvg}<span>Drunk</span>${botSvg}</button>
+           <button type='button' data-tone="joke" class='ai_button  text-sm rounded-lg py-1 px-2 border-2 border-gray-400 dark:border-gray-400 disabled:cursor-progress fill-gray-500'>${loaderSvg}<span>Joke</span>${jokeSvg}</button>
+           <button type='button' data-tone="mad" class='ai_button  text-sm rounded-lg py-1 px-2 border-2 border-gray-400 dark:border-gray-400 disabled:cursor-progress fill-gray-500'>${loaderSvg}<span>Angry</span>${madSvg}</button>
+           <button type='button' data-tone="drunk" class='ai_button  text-sm rounded-lg py-1 px-2 border-2 border-gray-400 dark:border-gray-400 disabled:cursor-progress fill-gray-500'>${loaderSvg}<span>Drunk</span>${drunkSvg}</button>
       </div>`
     );
 
